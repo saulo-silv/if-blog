bb182e4e1843a44f14a59f4b656542a50acdae5a
saulo-silv <vitorgabriel@gmail.com>
2023-06-14 21:31:37 -0300

    commit

--- src/main/java/com/io/blog/controller/AutorController.java
+++ src/main/java/com/io/blog/controller/AutorController.java
@@ -1,30 +1,37 @@
 package com.io.blog.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.io.blog.model.Autor;
 import com.io.blog.service.AutorService;
 
-
-@Controller
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
 @RequestMapping("/autor")
 public class AutorController {
 	
 	@Autowired
 	private AutorService serviceAutor;
-	@GetMapping("/formulario")
-	public String formulario() {
-		return "formulario";
-	}
 	
 	@PostMapping("/salvar")
-	public String salvar(Autor autor) {
-		serviceAutor.cadastrarAutor(autor);
-		return "sucesso";
+	public String salvar(@RequestBody Autor autor) {
+		try {
+			System.out.println(autor);
+			serviceAutor.cadastrarAutor(autor);
+			return "{\"status\":\"Usuário cadastrado com sucesso\"}";
+		} catch (Exception e) {
+			return "{\"status\":\"Erro no cadastro\"}";
+		}
+
 	}
 	
 }
+
